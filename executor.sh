11a483e374d265f5ac375d8cc95985dfe7ed537e
Setu Gupta <setu.gupta.2020@gmail.com>
2022-12-13 00:31:20 +0530

    Added automated data collection
    On branch simplified
    Your branch is up to date with 'origin/simplified'.
    
     Changes to be committed:
    	modified:   README
    	new file:   executor.sh

---
+++ executor.sh
@@ -0,0 +1,11 @@
+for plot in ./plot[1,2,3]
+do
+        cd $plot;
+        for decoder in ./*
+        do
+                cd $decoder;
+                python3 csv_gen.py 17 ./out.csv;
+                cd ..;
+        done
+        cd ..;
+done
